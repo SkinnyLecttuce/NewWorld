9078dbf35bde8c0659aba41b2e35e79c33edd3b8
Janitha Manatunga <janithamanatunga@gmail.com>
2026-07-16 19:49:14 +0530

    Revert "build(deps): revert to CommonJS and change to "type": "commonjs""
    This reverts commit 52e6647ebe5723c590d15a131b683cd830b409a2.

--- realtime-service/src/config/index.js
+++ realtime-service/src/config/index.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
 
-module.exports = {
+dotenv.config();
+
+export default {
     port: process.env.PORT || 4000,
     corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     nodeEnv: process.env.NODE_ENV || 'development', 
